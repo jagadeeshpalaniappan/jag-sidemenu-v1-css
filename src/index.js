3c4735e5dbfbc2712304fbb3763446de6fa33936
Jagadeesh Palaniappan <jagadeeshthegeek@gmail.com>
2019-11-29 04:03:26 -0800

    animation --todo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 import "./reset.css";
-import "./styles.css";
+import "./jag-sidemenu.scss";
 import "./tooltip.css";
+import "./app.scss";
 
 /*
-Array.from(Array(5), (x,idx)=> idx + 1 ).forEach(i => console.log(`<a href="#" class="sidemenu-body-list-item"><i class="material-icons">star</i>Submenu ${i}</a>`))
+Array.from(Array(5), (x,idx)=> idx + 1 ).forEach(i => console.log(`<a href="#" class="sidemenu-body-list-item"><i class="material-icons">star</i>Submenu ${i}</a>`)
 
+*/
 
-Icon Generation:
+/*
+
+NOW AS A SUPER EASY TO USE PLUGIN:
+https://panels.scotch.io
+
+*/
+
+document.querySelector(".sidemenu-main-menu").addEventListener("click", toggleSideMenu);
+function toggleSideMenu() {
+  document.querySelector(".sidemenu-sub-menu").classList.toggle("hide");
+}
+
+/*
+$(function() {
+  var special = ["reveal", "top", "boring", "perspective", "extra-pop"];
+
+  // Toggle Nav on Click
+  $("#hearts a").click(function() {
+    var transitionClass = $(this).data("transition");
+
+    if ($.inArray(transitionClass, special) > -1) {
+      $("body").removeClass();
+      $("body").addClass(transitionClass);
+    } else {
+      $("body").removeClass();
+      $("#site-canvas").removeClass();
+      $("#site-canvas").addClass(transitionClass);
+    }
+
+    $("#site-wrapper").toggleClass("show-nav");
+
+    return false;
+  });
+});
 
 */
