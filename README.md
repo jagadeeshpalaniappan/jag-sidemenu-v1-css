ab4b93b9ec439467693a3938ef6f82473c605fe9
Jagadeesh Palaniappan <jagadeeshthegeek@gmail.com>
2019-11-28 20:19:43 -0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# jag-sidemenu-v1-css
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./reset.css";
+import "./styles.css";
+import "./tooltip.css";
+
+/*
+Array.from(Array(5), (x,idx)=> idx + 1 ).forEach(i => console.log(`<a href="#" class="sidemenu-body-list-item"><i class="material-icons">star</i>Submenu ${i}</a>`))
+
+
+Icon Generation:
+
+*/
